ed7e132d0e4fd899a9ab8902fb023d85518962d6
MathewAddala <addalamathew@gmail.com>
2026-02-21 12:32:16 +0530

    Added Skill 4 - Spring Dependency Injection

---
+++ spring-di/src/main/java/com/skill4/annotation/AnnotationMain.java
@@ -0,0 +1,17 @@
+package com.skill4.annotation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AnnotationMain {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        Student student = context.getBean(Student.class);
+
+        student.display();
+    }
+}
---
+++ spring-di/src/main/java/com/skill4/annotation/AppConfig.java
@@ -0,0 +1,9 @@
+package com.skill4.annotation;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.skill4.annotation")
+public class AppConfig {
+}
